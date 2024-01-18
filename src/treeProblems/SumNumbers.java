9768082f294fe323746418b5d3000b24388eac7d
Ankit <ankitkr00.india@gmail.com>
2024-01-19 01:05:22 +0530

    sum number from root to leaf node of Binary Tree solution added

---
+++ src/treeProblems/SumNumbers.java
@@ -0,0 +1,41 @@
+package src.treeProblems;
+
+public class SumNumbers {
+    // https://leetcode.com/problems/sum-root-to-leaf-numbers/
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    class Solution {
+        public int sumNumbers(TreeNode root) {
+            if(root == null) return 0;
+            return getSum(root, 0);
+        }
+
+        private int getSum(TreeNode root, int num) {
+            if(root == null) {
+                return 0;
+            }
+            num = num * 10 + root.val;
+
+            if(root.left == null && root.right == null) {
+                return num;
+            }
+            int leftSum = getSum(root.left, num);
+            int rightSum = getSum(root.right, num);
+            return leftSum + rightSum;
+        }
+    }
+}
