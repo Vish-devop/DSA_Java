3f88dfb666b63f2ed4f2eea318c374bae713a64f
Ankit <ankitkr00.india@gmail.com>
2024-02-04 06:55:16 +0530

    976. Largest Perimeter Triangle

---
+++ src/assignments/sorting/LargestPerimeter.java
@@ -0,0 +1,16 @@
+package src.assignments.sorting;
+
+import java.util.Arrays;
+
+class LargestPerimeter {
+    // https://leetcode.com/problems/largest-perimeter-triangle/
+    public int largestPerimeter(int[] nums) {
+        Arrays.sort(nums);
+        for(int i = nums.length - 3; i >= 0; i--) {
+            if(nums[i] + nums[i + 1] > nums[i+2]) {
+                return nums[i] + nums[i + 1] + nums[i + 2];
+            }
+        }
+        return 0;
+    }
+}
